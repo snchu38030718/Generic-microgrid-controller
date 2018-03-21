5928b7a9be5342094f65971ce5a6286abee609c4
snchu38030718 <buaa_sun@outlook.com>
2018-03-20 20:15:02 -0400

    send

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ while 1:
      pid.SetPoint=0.0
      pid.setSampleTime(0.0000)
      command[3]=0 # default, no PI control
-#     if spent_time>10:  # setpoint change
-#            pid.SetPoint = 0.3 # Setpoint reference
-#            pid.update(feedback1) # update_feedback
-#            command[3] = pid.output  # output
+     if spent_time>10:  # setpoint change
+            pid.SetPoint = 0.3 # Setpoint reference
+            pid.update(feedback1) # update_feedback
+            command[3] = pid.output  # output
     
     
      # send back
