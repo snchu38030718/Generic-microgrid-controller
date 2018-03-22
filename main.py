84c968b3ab3abd2a14643260cd26e5d9463f0bfe
snchu38030718 <buaa_sun@outlook.com>
2018-03-21 23:13:44 -0400

    send

--- main.py
+++ main.py
@@ -67,11 +67,11 @@ while 1:
 #               time.sleep(0.001)   # time_sleep
                 print(command[4])
 
-#     if spent_time>11 and abs(feedback1)<=0.1: 
-#         command[3]=0
-#         command[4]=1
-#         print(command[4])
-#         flag=0      # flage is ued to lock the switch state
+     if spent_time>13 and abs(feedback1)<=0.1: 
+         command[3]=0
+         command[4]=1
+         print(command[4])
+         flag=0      # flage is ued to lock the switch state
 
      # send back
      command1=tuple(command)
