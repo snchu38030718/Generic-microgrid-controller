424f41fb4e2813bbde0d43ef5a534a8703029a70
snchu38030718 <buaa_sun@outlook.com>
2018-03-21 22:13:53 -0400

    send

--- main.py
+++ main.py
@@ -59,13 +59,13 @@ while 1:
                 command[3] = pid.output  # output
 #            #time.sleep(0.001)   # time_sleep
 
-#     if spent_time>10 and abs(feedback1)>=0.001:  # setpoint change
-#         if flag==1:
-#                pid.SetPoint = -0.1 # Setpoint reference
-#                pid.update(feedback1) # update_feedback
-#                command[3] = pid.output  # output
-            #time.sleep(0.001)   # time_sleep
-#                print(command[4])
+     if spent_time>15 and abs(feedback1)>=0.001:  # setpoint change
+         if flag==1:
+                pid.SetPoint = -0.1 # Setpoint reference
+                pid.update(feedback1) # update_feedback
+                command[3] = pid.output  # output
+#               time.sleep(0.001)   # time_sleep
+                print(command[4])
 
 #     if spent_time>11 and abs(feedback1)<=0.1: 
 #         command[3]=0
