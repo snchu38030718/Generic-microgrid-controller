3f9c9e56d8418449f94a2b6c8881bc80fcc14ce6
snchu38030718 <buaa_sun@outlook.com>
2018-03-22 14:28:22 -0400

    send

--- main.py
+++ main.py
@@ -110,8 +110,8 @@ while 1:
                  command[3]=0
                  
      tie_delay=time.time()-time_close
-     print (tie_delay)
      if (tie_delay)>=5 and ph_flag==0: # re-enable tie_line control 
+         print (tie_delay)
          command[4]=0             # keep closed
          pid.SetPoint = -0.2 # Setpoint reference
          pid.update(feedback1) # update_feedback
