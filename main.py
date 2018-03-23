92f07fd446396ddd13b5c10792e29f77fc833da8
snchu38030718 <buaa_sun@outlook.com>
2018-03-22 20:56:03 -0400

    send

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ while 1:
                  command[4]=0
                  command[3]=0
 #                 pid.clear
-                 pid1=PID.PID(P=0.001, I=1000000, D=0.000)
+                 pid1=PID.PID(P=0.001, I=1200000, D=0.000)
                  ph_flag=0  # log close state
                  time_close=time.time()
             elif ph_flag==1:  # keep open
@@ -103,7 +103,7 @@ while 1:
                  command[3]=0
                  ph_flag=0
 #                 pid.clear
-                 pid1=PID.PID(P=0.001, I=1000000, D=0.000)
+                 pid1=PID.PID(P=0.001, I=1200000, D=0.000)
                  time_close=time.time()
             elif ph_flag==1:  # keep open
                  command[4]=1
