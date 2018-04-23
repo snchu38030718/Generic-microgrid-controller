e8b5aec768b4d1e2ecb877103df1d17e791f0a4d
snchu38030718 <buaa_sun@outlook.com>
2018-04-23 11:04:29 -0400

    send

--- main.py
+++ main.py
@@ -45,6 +45,9 @@ tie_flag=1
 time_delay=0
 Pdiesel1=0
 P_ES1=0
+save0=0
+save1=0
+save2=0
 while 1:
      start_time = time.time()
      command=list(m.e.status())
@@ -83,7 +86,7 @@ while 1:
                 
 ################################################################################
 #### grid-connected dispatch
-     if spent_time>22 and spent_time<=60:
+     if spent_time>22:
         if flag==1:
             gdispatch=Gridisp.Gridisp()
             SoC=command[3]
@@ -94,8 +97,10 @@ while 1:
             command[0]=gdispatch.Pdsref
             command[1]=gdispatch.Pwdref
             command[2]=gdispatch.Pldref
-            command[4]=1            # ess changes to Vf control
-            command[3]=0
+            pid.SetPoint = -0.2 # Setpoint reference
+            pid.update(feedback1) # update_feedback
+            command[3] = pid.output  # output
+            command[4]=0            # ess changes to Vf control
             command[5]=0
             command[6]=0
             StartDs=gdispatch.Start_ds    
@@ -103,147 +108,147 @@ while 1:
 ###############################################################################
                 
 
-     if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
-         if flag==1:
-                pid.SetPoint = 0 # Setpoint reference
-                pid.update(feedback1) # update_feedback
-                command[3] = pid.output  # output
-                save_pess=command[3]
-                command[4]=0
-                command[0]=0
-                command[1]=0
-                command[2]=0
-#               time.sleep(0.001)   # time_sleep
-                #print(command[4])
-
-     if (spent_time>=65 and abs(feedback1)<=0.01 and ph_flag==1) or flag==0: # open breaker
-         flag=0      # flag is ued to lock the open state
-         command[4]=1
-         command[3]=0
-         command[0]=0
-         command[1]=0
-         command[2]=0
-#         pid.clear
-         #print(command[4])
-         
-        
-     if spent_time>80 and tie_flag==1:           # phase-check and synchronization
-         print (ph_chck)
-         if command[2]>=0.5:
-            if ph_chck>=ph_min1 and ph_chck<=ph_max1 and ph_flag==1: # close breaker
-                 command[4]=0
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-                 pid.clear
-#                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
-                 ph_flag=0  # log close state
-                 time_close=time.time()
-            elif ph_flag==1:  # keep open
-                 command[4]=1
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-                 pid.clear
-            elif ph_flag==0:             # keep closed
-                 command[4]=0
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-         else:
-            if ph_chck>=ph_min and ph_chck<=ph_max and ph_flag==1: # close breaker
-                 command[4]=0
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-                 ph_flag=0
-                 pid.clear
-#                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
-#                 pid1.clear
-                 time_close=time.time()
-            elif ph_flag==1:  # keep open
-                 command[4]=1
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-                 pid.clear
-            elif ph_flag==0:             # keep closed
-                 command[4]=0
-                 command[3]=0
-                 command[0]=0
-                 command[1]=0
-                 command[2]=0
-                 
-     tie_delay=time.time()-time_close
-     if (tie_delay)>=8 and ph_flag==0 and spent_time<=120: # re-enable tie_line control 
-         #print (tie_delay)
-         command[4]=0             # keep closed, PQ control
-#         print(feedback1)
-         pid.setSampleTime(0.00)
-         pid.SetPoint = -0.5 # Setpoint reference
-         pid.update(feedback1) # update_feedback
-         command[3] = pid.output  # output
-         command[0]=0
-         command[1]=0
-         command[2]=0
-         save_pess=command[3]
+#     if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
+#         if flag==1:
+#                pid.SetPoint = 0 # Setpoint reference
+#                pid.update(feedback1) # update_feedback
+#                command[3] = pid.output  # output
+#                save_pess=command[3]
+#                command[4]=0
+#                command[0]=0
+#                command[1]=0
+#                command[2]=0
+##               time.sleep(0.001)   # time_sleep
+#                #print(command[4])
+#
+#     if (spent_time>=65 and abs(feedback1)<=0.01 and ph_flag==1) or flag==0: # open breaker
+#         flag=0      # flag is ued to lock the open state
+#         command[4]=1
 #         command[3]=0
-         tie_flag=0
-     
-     
-###############################################################################       
- # Unplanned islanding 
-     if spent_time>120 and spent_time<120.01 and tie_flag==0:  # change power reference
-         unplan=Unplan.Unplan()
-         unplan.edispatch(Pdiesel1, P_ES1)
-         command[0]=unplan.dPdiesel
-         save0=command[0]
-         command[1]=unplan.PCwd
-         save1=command[1]
-         command[2]=unplan.PSLd
-         save2=command[2]
-         command[4]=0            # ess stays at PQ control
-         command[3]=save_pess    # ess is the power reference change of ess
-
-     if spent_time>=120.01 and spent_time<=125:                                # change ESS mode
-#            unplan=Unplan.Unplan()
-#            Pdiesel1=command[1]
-#            P_ES1=-command[3]
-#            unplan.edispatch(Pdiesel1, P_ES1)
-#            command[0]=unplan.dPdiesel
-#            command[1]=unplan.PCwd
-#            command[2]=unplan.PSLd
-            command[0]=save0
-            command[1]=save1
-            command[2]=save2
-            command[4]=1            # ess changes to Vf control
-            command[3]=0
-            StartDs=1
-                    
-###############################################################################       
-        
-        # Pwdref,Pdsref,Pldref,Start_ds
-  ###############################################################################       
- # Island dispatch
-     if spent_time>125:
-        dispatch=Isldisp.Isldisp()
-        SoC=command[3]
-        Pwind=command[5]
-        Pload=command[6]
-        dispatch.isldispatch(Pwind,Pload,SoC,StartDs)
-        command[0]=dispatch.Pdsref
-        command[1]=dispatch.Pwdref
-        command[2]=dispatch.Pldref
-        command[4]=1            # ess changes to Vf control
-        command[3]=0
-        command[5]=0
-        command[6]=0
-        StartDs=dispatch.Start_ds
+#         command[0]=0
+#         command[1]=0
+#         command[2]=0
+##         pid.clear
+#         #print(command[4])
+#         
+#        
+#     if spent_time>80 and tie_flag==1:           # phase-check and synchronization
+#         print (ph_chck)
+#         if command[2]>=0.5:
+#            if ph_chck>=ph_min1 and ph_chck<=ph_max1 and ph_flag==1: # close breaker
+#                 command[4]=0
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#                 pid.clear
+##                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
+#                 ph_flag=0  # log close state
+#                 time_close=time.time()
+#            elif ph_flag==1:  # keep open
+#                 command[4]=1
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#                 pid.clear
+#            elif ph_flag==0:             # keep closed
+#                 command[4]=0
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#         else:
+#            if ph_chck>=ph_min and ph_chck<=ph_max and ph_flag==1: # close breaker
+#                 command[4]=0
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#                 ph_flag=0
+#                 pid.clear
+##                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
+##                 pid1.clear
+#                 time_close=time.time()
+#            elif ph_flag==1:  # keep open
+#                 command[4]=1
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#                 pid.clear
+#            elif ph_flag==0:             # keep closed
+#                 command[4]=0
+#                 command[3]=0
+#                 command[0]=0
+#                 command[1]=0
+#                 command[2]=0
+#                 
+#     tie_delay=time.time()-time_close
+#     if (tie_delay)>=8 and ph_flag==0 and spent_time<=120: # re-enable tie_line control 
+#         #print (tie_delay)
+#         command[4]=0             # keep closed, PQ control
+##         print(feedback1)
+#         pid.setSampleTime(0.00)
+#         pid.SetPoint = -0.5 # Setpoint reference
+#         pid.update(feedback1) # update_feedback
+#         command[3] = pid.output  # output
+#         command[0]=0
+#         command[1]=0
+#         command[2]=0
+#         save_pess=command[3]
+##         command[3]=0
+#         tie_flag=0
+#     
+#     
+################################################################################       
+# # Unplanned islanding 
+#     if spent_time>120 and spent_time<120.01 and tie_flag==0:  # change power reference
+#         unplan=Unplan.Unplan()
+#         unplan.edispatch(Pdiesel1, P_ES1)
+#         command[0]=unplan.dPdiesel
+#         save0=command[0]
+#         command[1]=unplan.PCwd
+#         save1=command[1]
+#         command[2]=unplan.PSLd
+#         save2=command[2]
+#         command[4]=0            # ess stays at PQ control
+#         command[3]=save_pess    # ess is the power reference change of ess
+#
+#     if spent_time>=120.01 and spent_time<=125:                                # change ESS mode
+##            unplan=Unplan.Unplan()
+##            Pdiesel1=command[1]
+##            P_ES1=-command[3]
+##            unplan.edispatch(Pdiesel1, P_ES1)
+##            command[0]=unplan.dPdiesel
+##            command[1]=unplan.PCwd
+##            command[2]=unplan.PSLd
+#            command[0]=save0
+#            command[1]=save1
+#            command[2]=save2
+#            command[4]=1            # ess changes to Vf control
+#            command[3]=0
+#            StartDs=1
+#                    
+################################################################################       
+#        
+#        # Pwdref,Pdsref,Pldref,Start_ds
+#  ###############################################################################       
+# # Island dispatch
+#     if spent_time>125:
+#        dispatch=Isldisp.Isldisp()
+#        SoC=command[3]
+#        Pwind=command[5]
+#        Pload=command[6]
+#        dispatch.isldispatch(Pwind,Pload,SoC,StartDs)
+#        command[0]=dispatch.Pdsref
+#        command[1]=dispatch.Pwdref
+#        command[2]=dispatch.Pldref
+#        command[4]=1            # ess changes to Vf control
+#        command[3]=0
+#        command[5]=0
+#        command[6]=0
+#        StartDs=dispatch.Start_ds
  
 ###############################################################################         
         
