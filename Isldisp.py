e9901f8b90c107e037dff201192374776a3f3fc7
snchu38030718 <buaa_sun@outlook.com>
2018-04-22 22:55:26 -0400

    send

--- Isldisp.py
+++ Isldisp.py
@@ -20,6 +20,15 @@ class Isldisp:
         self.Pldref=0
 
     def isldispatch(self,Pwind,Pload,SoC,start_ds):  ## P_ES is power at POI
+        self.SoC_max = 0.9   #95
+        self.SoC_min = 0.2
+        self.Pch_max = 1
+        self.Pdis_max = -1  #-1.5
+        self.Pds_min=0.2
+        self.Pds_max=1
+        self.Pwdref=0
+        self.Pdsref=0
+        self.Pldref=0
         Pnet = Pwind-Pload
         if Pnet>=0:    # More, wind is controllable, diesel is off, ESS depends
                 if SoC>=self.SoC_max:     # Charge is
@@ -59,7 +68,7 @@ class Isldisp:
                             self.Pwdref=Pnet-self.Pch_max
                             self.Pdsref=0
                             self.Pldref=0   
-        elif SoC>=0.2:    ## Pnet<0, load is controllable, diesel on/off, 
+        elif SoC>=0.2:    ## Pnet<0, load is controllable, diesel on/off, SoC_min
                 if  start_ds>=1 and start_ds<=3:
                        self.Pdsref=self.Pds_min
                        Pnet=Pwind+self.Pds_min-Pload
@@ -93,7 +102,7 @@ class Isldisp:
                             self.Pwdref=0
                
         else:            ## SoC<self.SoC_min, diesel should be on
-                 if -Pnet<self.Pds_min:  # Pnet is smaller than the smallest diesel power
+                 if -Pnet<self.Pds_min:  # Pnet is smaller than the smallest diesel power Pds_min
                         if  start_ds>=1 and start_ds<=3:
                             self.Pdsref=self.Pds_min
                             self.Start_ds=start_ds+1
