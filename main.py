bcacdbbee7af3fe9f75c58036f1ecb02bb702bc5
snchu38030718 <buaa_sun@outlook.com>
2018-04-24 19:58:08 -0400

    send

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ while 1:
 #     
 #################################################################################       
  # Unplanned islanding 
-     if spent_time>130 and spent_time<130.01 and tie_flag==0:  # change power reference
+     if spent_time>130 and spent_time<130.1 and tie_flag==0:  # change power reference
          unplan=Unplan.Unplan()
          unplan.edispatch(Pdiesel1, P_ES1)
          command[0]=unplan.dPdiesel
@@ -248,7 +248,7 @@ while 1:
          command[4]=0            # ess stays at PQ control
          command[3]=save_pess    # ess is the power reference change of ess
 
-     if spent_time>=130.01 and spent_time<=135:                                # change ESS mode
+     if spent_time>=130.1 and spent_time<=135:                                # change ESS mode
 #            unplan=Unplan.Unplan()
 #            Pdiesel1=command[1]
 #            P_ES1=-command[3]
