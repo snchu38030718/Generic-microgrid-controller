159cc94e906be5dd18ccd582d2ece5245d22e636
snchu38030718 <buaa_sun@outlook.com>
2018-04-25 10:29:30 -0400

    send

--- main.py
+++ main.py
@@ -119,131 +119,131 @@ while 1:
 #            global save_pess
             save_pess=command[3] 
 
+##############################################################################
+ ##### planned islanding               
+     if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
+         pid = PID.PID(P=0.005, I=10000, D=0.000)  # give P,I,D, but not update now
+         if flag==1:
+                gdispatch=Gridisp.Gridisp()
+                SoC=command[0]
+                Pwind=command[5]
+                Pload=command[6]
+                PES=0
+                gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
+                command[0]=gdispatch.Pdsref
+                save0=command[0]
+                command[1]=gdispatch.Pwdref
+                save1=command[1]
+                command[2]=gdispatch.Pldref
+                save2=command[2]
+                StartDs=gdispatch.Start_ds
+                pid.SetPoint = 0 # Setpoint reference
+                pid.update(feedback1) # update_feedback
+                command[3] = pid.output  # output
+                save_pess=command[3]
+                command[4]=0
+#                command[0]=0
+#                command[1]=0
+#                command[2]=0
+#               time.sleep(0.001)   # time_sleep
+                #print(command[4])
+
+     if (spent_time>=65 and abs(feedback1)<=0.01 and ph_flag==1) or flag==0: # open breaker
+         flag=0      # flag is ued to lock the open state
+         command[4]=1
+         command[3]=0
+         command[0]=save0
+         command[1]=save1
+         command[2]=save2
+#         pid.clear
+         #print(command[4])
+         
 ###############################################################################
-# ##### planned islanding               
-#     if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
-#         pid = PID.PID(P=0.005, I=10000, D=0.000)  # give P,I,D, but not update now
-#         if flag==1:
-#                gdispatch=Gridisp.Gridisp()
-#                SoC=command[0]
-#                Pwind=command[5]
-#                Pload=command[6]
-#                PES=0
-#                gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
-#                command[0]=gdispatch.Pdsref
-#                save0=command[0]
-#                command[1]=gdispatch.Pwdref
-#                save1=command[1]
-#                command[2]=gdispatch.Pldref
-#                save2=command[2]
-#                StartDs=gdispatch.Start_ds
-#                pid.SetPoint = 0 # Setpoint reference
-#                pid.update(feedback1) # update_feedback
-#                command[3] = pid.output  # output
-#                save_pess=command[3]
-#                command[4]=0
-##                command[0]=0
-##                command[1]=0
-##                command[2]=0
-##               time.sleep(0.001)   # time_sleep
-#                #print(command[4])
-#
-#     if (spent_time>=65 and abs(feedback1)<=0.01 and ph_flag==1) or flag==0: # open breaker
-#         flag=0      # flag is ued to lock the open state
-#         command[4]=1
+##### Reconnection
+     if spent_time>100 and tie_flag==1:           # phase-check and synchronization
+         print (ph_chck)
+         if command[2]>=0.5:               # phase_difference
+            if ph_chck>=ph_min1 and ph_chck<=ph_max1 and ph_flag==1: # close breaker
+                 command[4]=0
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+                 pid.clear
+#                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
+                 ph_flag=0  # log close state
+                 time_close=time.time()
+            elif ph_flag==1:  # keep open
+                 command[4]=1
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+                 pid.clear
+            elif ph_flag==0:             # keep closed
+                 command[4]=0
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+         else:
+            if ph_chck>=ph_min and ph_chck<=ph_max and ph_flag==1: # close breaker
+                 command[4]=0
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+                 ph_flag=0
+                 pid.clear
+#                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
+#                 pid1.clear
+                 time_close=time.time()
+            elif ph_flag==1:  # keep open
+                 command[4]=1
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+                 pid.clear
+            elif ph_flag==0:             # keep closed
+                 command[4]=0
+                 command[3]=0
+                 command[0]=save0
+                 command[1]=save1
+                 command[2]=save2
+
+###############################################################################               
+### reenable tie_line control  
+     tie_delay=time.time()-time_close
+     if (tie_delay)>=8 and ph_flag==0 and spent_time<=130: # re-enable tie_line control 
+         #print (tie_delay)
+         command[4]=0             # keep closed, PQ control
+#         print(feedback1)
+         gdispatch=Gridisp.Gridisp()
+         SoC=command[0]
+         Pwind=command[5]
+         Pload=command[6]
+         PES=0.5
+         gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
+         command[0]=gdispatch.Pdsref
+         save0=command[0]
+         command[1]=gdispatch.Pwdref
+         save1=command[1]
+         command[2]=gdispatch.Pldref
+         save2=command[2]
+         StartDs=gdispatch.Start_ds
+         pid.setSampleTime(0.00)
+         pid.SetPoint = -0.5 # Setpoint reference
+         pid.update(feedback1) # update_feedback
+         command[3] = pid.output  # output
+         save_pess=command[3]
 #         command[3]=0
-#         command[0]=save0
-#         command[1]=save1
-#         command[2]=save2
-##         pid.clear
-#         #print(command[4])
-#         
-################################################################################
-###### Reconnection
-#     if spent_time>100 and tie_flag==1:           # phase-check and synchronization
-#         print (ph_chck)
-#         if command[2]>=0.5:               # phase_difference
-#            if ph_chck>=ph_min1 and ph_chck<=ph_max1 and ph_flag==1: # close breaker
-#                 command[4]=0
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#                 pid.clear
-##                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
-#                 ph_flag=0  # log close state
-#                 time_close=time.time()
-#            elif ph_flag==1:  # keep open
-#                 command[4]=1
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#                 pid.clear
-#            elif ph_flag==0:             # keep closed
-#                 command[4]=0
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#         else:
-#            if ph_chck>=ph_min and ph_chck<=ph_max and ph_flag==1: # close breaker
-#                 command[4]=0
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#                 ph_flag=0
-#                 pid.clear
-##                 pid1=PID.PID(P=0.01, I=1000000, D=0.000)
-##                 pid1.clear
-#                 time_close=time.time()
-#            elif ph_flag==1:  # keep open
-#                 command[4]=1
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#                 pid.clear
-#            elif ph_flag==0:             # keep closed
-#                 command[4]=0
-#                 command[3]=0
-#                 command[0]=save0
-#                 command[1]=save1
-#                 command[2]=save2
-#
-################################################################################               
-#### reenable tie_line control  
-#     tie_delay=time.time()-time_close
-#     if (tie_delay)>=8 and ph_flag==0 and spent_time<=130: # re-enable tie_line control 
-#         #print (tie_delay)
-#         command[4]=0             # keep closed, PQ control
-##         print(feedback1)
-#         gdispatch=Gridisp.Gridisp()
-#         SoC=command[0]
-#         Pwind=command[5]
-#         Pload=command[6]
-#         PES=0.5
-#         gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
-#         command[0]=gdispatch.Pdsref
-#         save0=command[0]
-#         command[1]=gdispatch.Pwdref
-#         save1=command[1]
-#         command[2]=gdispatch.Pldref
-#         save2=command[2]
-#         StartDs=gdispatch.Start_ds
-#         pid.setSampleTime(0.00)
-#         pid.SetPoint = -0.5 # Setpoint reference
-#         pid.update(feedback1) # update_feedback
-#         command[3] = pid.output  # output
-#         save_pess=command[3]
-##         command[3]=0
-#         tie_flag=0
-##     
+         tie_flag=0
 #     
+     
 #################################################################################       
  # Unplanned islanding 
-     if spent_time>130 and spent_time<132 and tie_flag==1:  # change power reference
+     if spent_time>130 and spent_time<132 and tie_flag==0:  # change power reference
          unplan.edispatch(Pdiesel1, P_ES1)
          command[0]=unplan.dPdiesel
 #         global save0
