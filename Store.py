ed41375d3d3042e456a5ed5855a440d3926737e0
snchu38030718 <buaa_sun@outlook.com>
2018-04-24 22:47:30 -0400

    send

--- Store.py
+++ Store.py
@@ -7,7 +7,7 @@ Created on Tue Apr 24 22:09:25 2018
 
 class Store( object ):
     def __init__( self ):
-        self.x, self.y, self.z = None, None, None
+        self.x, self.y, self.z = 1.0, 1.0, 3.0
     def store(self, x,y,z):
 #        if x is None and y is None and z is None:
 #            return self.x, self.y, self.z
