eae3ffcaf052738f04f2dd9717782ed0a77c0aac
snchu38030718 <buaa_sun@outlook.com>
2018-04-24 20:48:25 -0400

    send

--- main.py
+++ main.py
@@ -45,11 +45,10 @@ tie_flag=1
 time_delay=0
 Pdiesel1=0
 P_ES1=0
-#global save0
-save0=0
-save1=0
-save2=0
-StartDs=1
+global save0
+global save1
+global save2
+global StartDs
 savepess=0
 while 1:
      start_time = time.time()
