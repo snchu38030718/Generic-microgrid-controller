75e7891f591052065c2a9d6d8bc0ac39154cb7af
snchu38030718 <buaa_sun@outlook.com>
2018-04-28 12:10:02 -0400

    send

--- main.py
+++ main.py
@@ -113,39 +113,39 @@ while 1:
                 
 ################################################################################
 #### grid-connected dispatch
-     if spent_time>41 and spent_time<=60:
-#        command=list(m.e.status())
-        if flag==1:
-            gdispatch=Gridisp.Gridisp()
-            SoC=command[0]
-            print(SoC)
-            Pwind=command[5]
-            Pload=command[6]
-            PES=0.2
-            gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
-            command[0]=gdispatch.Pdsref
-            command[1]=gdispatch.Pwdref
-            command[2]=gdispatch.Pldref
-            pid = PID.PID(P=0.01, I=1000000, D=0.000)
-            pid.SetPoint = -0.2 # Setpoint reference
-            pid.update(feedback1) # update_feedback
-            command[3] = pid.output  # output
-            command[4]=0            
-            command[5]=0
-            command[6]=0
-            StartDs=gdispatch.Start_ds
-#            global save_pess
-            save_pess=command[3] 
-#            command[3]=0# output
-#            command[4]=0
-#            command[0]=0.2
-#            command[1]=0
-#            command[2]=0
-#            StartDs=1
+#     if spent_time>41 and spent_time<=60:
+##        command=list(m.e.status())
+#        if flag==1:
+#            gdispatch=Gridisp.Gridisp()
+#            SoC=command[0]
+#            print(SoC)
+#            Pwind=command[5]
+#            Pload=command[6]
+#            PES=0.2
+#            gdispatch.gridispatch(Pwind,Pload,SoC,PES,StartDs)
+#            command[0]=gdispatch.Pdsref
+#            command[1]=gdispatch.Pwdref
+#            command[2]=gdispatch.Pldref
+#            pid = PID.PID(P=0.01, I=1000000, D=0.000)
+#            pid.SetPoint = -0.2 # Setpoint reference
+#            pid.update(feedback1) # update_feedback
+#            command[3] = pid.output  # output
+#            command[4]=0            
+#            command[5]=0
+#            command[6]=0
+#            StartDs=gdispatch.Start_ds
+##            global save_pess
+#            save_pess=command[3] 
+##            command[3]=0# output
+##            command[4]=0
+##            command[0]=0.2
+##            command[1]=0
+##            command[2]=0
+##            StartDs=1
 
 ##############################################################################
  ##### planned islanding               
-     if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
+     if spent_time>41 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
          pid = PID.PID(P=0.01, I=1000000, D=0.000)  # give P,I,D, but not update now
          command=list(m.e.status())
          if flag==1:
