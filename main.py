200c05ec77f46039ed6965ca785cd1b244ca1557
snchu38030718 <buaa_sun@outlook.com>
2018-04-28 16:39:32 -0400

    send

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ while 1:
 ##############################################################################
  ##### planned islanding               
      if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
-         pid = PID.PID(P=0.01, I=10000, D=0.000)  # give P,I,D, but not update now
+         pid = PID.PID(P=0.05, I=10000, D=0.000)  # give P,I,D, but not update now
 #         command=list(m.e.status())
          if flag==1:
                 gdispatch=Gridisp.Gridisp()
