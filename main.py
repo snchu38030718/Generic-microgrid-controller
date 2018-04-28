a1c5e4d624fd0b66d7bc63e4b047b2c573990af9
snchu38030718 <buaa_sun@outlook.com>
2018-04-28 16:53:53 -0400

    send

--- main.py
+++ main.py
@@ -126,10 +126,12 @@ while 1:
             command[0]=gdispatch.Pdsref
             command[1]=gdispatch.Pwdref
             command[2]=gdispatch.Pldref
-            pid = PID.PID(P=0.01, I=500000, D=0.000)
+            pid = PID.PID(P=0.01, I=100000, D=0.000)
             pid.SetPoint = -0.2 # Setpoint reference
             pid.update(feedback1) # update_feedback
             command[3] = pid.output  # output
+            if SoC<0.2 and pid.output<0:
+                command[3]=0
             command[4]=0            
             command[5]=0
             command[6]=0
@@ -147,7 +149,7 @@ while 1:
 ##############################################################################
  ##### planned islanding               
      if spent_time>60 and abs(feedback1)>=0.001 and ph_flag==1:  # setpoint change
-         pid = PID.PID(P=0.05, I=10000, D=0.000)  # give P,I,D, but not update now
+         pid = PID.PID(P=0.01, I=10000, D=0.000)  # give P,I,D, but not update now
 #         command=list(m.e.status())
          if flag==1:
                 gdispatch=Gridisp.Gridisp()
