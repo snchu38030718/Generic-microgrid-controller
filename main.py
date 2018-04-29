f7d7834747421abbf30005f73a8053fee3dabf56
snchu38030718 <buaa_sun@outlook.com>
2018-04-28 22:32:52 -0400

    send

--- main.py
+++ main.py
@@ -342,6 +342,8 @@ while 1:
 #                pid.update(feedback1) # update_feedback
 #                print(pid.ITerm)
          command[3] = output # output
+         if SoC<0.2 and pid.output>0:
+                command[3]=0
          command[0]=save00
          command[1]=save11
          command[2]=save22
